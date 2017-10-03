5d25a36635a060cb016c0a08538b6ca1a7ac0d66
KyleYue <KyleYue@users.noreply.github.com>
2017-10-03 18:13:12 -0500

    More implementation on both front and back end (#18)
    * made a backend interface for the project
    
    * update README
    
    * saving progress, datastore works now
    
    * initial view page
    
    * view page works, still need improvement
    
    * Added html to create and manage page, some backend support for those two pages are still missing

--- connexus.py
+++ connexus.py
@@ -7,6 +7,8 @@ import model
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
+from google.appengine.api import mail
+
 
 import jinja2
 import webapp2
@@ -18,6 +20,9 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 # [END imports]
 
 # [START mainlogin page]
+DEFAULT_STREAM_NAME = 'new stream'
+
+
 
 class MainLoginPage(webapp2.RequestHandler):
 
@@ -41,25 +46,45 @@ class MainLoginPage(webapp2.RequestHandler):
         self.response.write(template.render(template_values))
 # [END mainlogin page]
 
+
 # [START manage page]
 class ManagePage(webapp2.RequestHandler):
 
     def get(self):
+        user = users.get_current_user()
+        streams = model.get_stream_by_user(user.user_id())
+        sub_streams = model.get_subscribed_stream(user.user_id())
+
         template_value = {
-            'greeting': 'this is the manage page'
+            'streams': streams,
+            'sub_streams': sub_streams,
         }
-        template = JINJA_ENVIRONMENT.get_template('templates/page_template.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/manage_page.html')
         self.response.write(template.render(template_value))
 # [END manage page]
+
 
 # [START create page]
 class CreatePage(webapp2.RequestHandler):
 
-    def get(self):
+    def post(self):
+        user = users.get_current_user()
+        stream_name = self.request.get('streamname', DEFAULT_STREAM_NAME)
+        sub = self.request.get('sub', '')
+        tag = self.request.get('tag', '')
+        model.create_stream(stream_name, sub, tag, user.user_id())
+
         template_value = {
             'greeting': 'this is the create page'
         }
         template = JINJA_ENVIRONMENT.get_template('templates/page_template.html')
+        self.response.write(template.render(template_value))
+
+    def get(self):
+        template_value = {
+            'greeting': 'this is the the create page'
+        }
+        template = JINJA_ENVIRONMENT.get_template('templates/create_page.html')
         self.response.write(template.render(template_value))
 # [END create page]
 
@@ -67,11 +92,29 @@ class CreatePage(webapp2.RequestHandler):
 # [START view page]
 class ViewPage(webapp2.RequestHandler):
 
-    def get(self):
+    def post(self):
+        user = users.get_current_user()
+        stream_name = self.request.get('steamname', DEFAULT_STREAM_NAME)
+        comment = self.request.get('comment')
+        title = self.request.get('title')
+        content = self.request.get('img')
+        model.add_photo(user.user_id(), stream_name, title, comment, content)
+        photo_ids = model.get_photo_id_for_user(user.user_id())
         template_value = {
-            'greeting': 'this is the view page'
+            'greeting': 'this is the view page',
+            'img_ids': photo_ids
         }
-        template = JINJA_ENVIRONMENT.get_template('templates/page_template.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/view_page.html')
+        self.response.write(template.render(template_value))
+
+    def get(self):
+        user = users.get_current_user()
+        photo_ids = model.get_photo_id_for_user(user.user_id())
+        template_value = {
+            'greeting': 'this is the view page',
+            'img_ids': photo_ids
+        }
+        template = JINJA_ENVIRONMENT.get_template('templates/view_page.html')
         self.response.write(template.render(template_value))
 # [END view page]
 
@@ -85,6 +128,7 @@ class SearchPage(webapp2.RequestHandler):
         }
         template = JINJA_ENVIRONMENT.get_template('templates/page_template.html')
         self.response.write(template.render(template_value))
+
 # [END search page]
 
 
@@ -99,6 +143,7 @@ class TrendingPage(webapp2.RequestHandler):
         self.response.write(template.render(template_value))
 # [END trending page]
 
+
 # [START social page]
 class SocialPage(webapp2.RequestHandler):
 
@@ -109,6 +154,7 @@ class SocialPage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('templates/page_template.html')
         self.response.write(template.render(template_value))
 # [END social page]
+
 
 # [START error page]
 class ErrorPage(webapp2.RequestHandler):
@@ -121,6 +167,21 @@ class ErrorPage(webapp2.RequestHandler):
         self.response.write(template.render(template_value))
 # [END error page]
 
+
+class Image(webapp2.RequestHandler):
+
+    def get(self):
+        user = users.get_current_user()
+        img_id = self.request.get('img_id')
+        if img_id:
+            print(img_id)
+            photo = model.get_photo(user.user_id(), img_id)
+            self.response.headers['Content-Type'] = 'image/png'
+            self.response.out.write(photo.content)
+        else:
+            self.response.out.write('No image')
+
+
 # [START app]
 app = webapp2.WSGIApplication([
     ('/', MainLoginPage),
@@ -130,6 +191,7 @@ app = webapp2.WSGIApplication([
     ('/search', SearchPage),
     ('/trending', TrendingPage),
     ('/social', SocialPage),
-    ('/error', ErrorPage)
+    ('/error', ErrorPage),
+    ('/image', Image)
 ], debug=True)
 # [END app]
--- model.py
+++ model.py
@@ -1,78 +1,103 @@
 from google.appengine.api import images
 from google.appengine.ext import ndb
 import os
-import lib.cloudstorage as gcs
 import webapp2
 
 from google.appengine.api import app_identity
 
 class Photo(ndb.Model):
+    owner = ndb.StringProperty()
+    img_id = ndb.IntegerProperty()
     stream_name = ndb.StringProperty()
     title = ndb.StringProperty()
     comment = ndb.StringProperty()
     last_update = ndb.DateTimeProperty(auto_now_add=True)
-
-    # calls the save_photo_to_cs method
-    def get_photo(self):
-        pass  # TODO
-
-    # calls the get_photo_from_cs method
-    def save_photo(self):
-        pass  # TODO
-
-    def get_comment(self):
-        pass  # TODO
-
-    def get_title(self):
-        pass  # TODO
-
-    def get_last_update(self):
-        pass  # TODO
-
-    def stream_name(self):
-        pass  # TODO
-
-    #internal method dealing with connection to data store
-    def save_photo_to_cs(self):
-        pass  # TODO
-
-    # internal method dealing with connection to data store
-    def get_photo_from_cs(self):
-        pass  # TODO
+    content = ndb.BlobProperty()
 
 
 class Stream(ndb.Model):
-    user_id = ndb.StringProperty()
     # stream_name is unique meaning
     stream_name = ndb.StringProperty()
-    image_count = ndb.IntegerProperty()
     last_update = ndb.DateTimeProperty(auto_now_add=True)
     tag = ndb.StringProperty()
-    photo_list = ndb.StructureProperty(Photo, repeated=True)
+    photo_counts = ndb.IntegerProperty()
 
-    def get_user_id(self):
-        pass  # TODO
 
-    def get_stream_name(self):
-        pass  # TODO
+class User(ndb.Model):
+    user_id = ndb.StringProperty()
+    stream_list = ndb.StructuredProperty(Stream, repeated=True)
+    subscribe_list = ndb.StringProperty(repeated=True)
+    photo_counts = ndb.IntegerProperty()
 
-    def get_image_count(self):
-        pass  # TODO
 
-    def get_last_update(self):
-        pass  # TODO
+def get_user(id):
+    curr_user = User.query(User.user_id == id)
+    user = curr_user.get()
+    if not user:
+        user = User()
+        user.user_id=id
+        user.subscribe_list = []
+        user.stream_list = []
+        user.photo_counts = 0
+    return user
 
-    def get_tag(self):
-        pass  # TODO
 
-    #saves photo to this stream, calls the save_photo method of Photo
-    def save_photo_to_stream(self):
-        pass  # TODO
+def get_stream_by_user(id):
+    user = get_user(id)
+    return user.stream_list
 
-    # gets photo from this stream, calls the get_photo method of Photo
-    def get_photo_from_stream(self):
-        pass  # TODO
 
-    #should return a list of photo object for display
-    def get_all_photo(self):
-        pass  # TODO
+def get_subscribed_stream(id):
+    user = get_user(id)
+    list = []
+    for sub_id in user.subscribe_list:
+        sub = get_user(sub_id)
+        list.append(sub.stream_list)
+    return list
+
+
+def get_photo_by_stream(stream_name, user_id):
+    photos = Photo.query(Photo.stream_name == stream_name, Photo.owner == user_id)
+    photo_list = []
+    for photo in photos.fetch():
+        photo_list.append(photo.content)
+    return photo_list
+
+
+def create_stream(stream_name, sub, tag, user_id):
+    user_data = get_user(user_id)
+    new_stream = Stream()
+    new_stream.tag = tag
+    new_stream.user_id = user_id
+    new_stream.stream_name = stream_name
+    user_data.stream_list.append(new_stream)
+    user_data.subscribe_list.append(sub)
+    user_data.put()
+
+
+def add_photo(user_id, stream_name, title, comment, content):
+    user_data = get_user(user_id)
+    img_id = user_data.photo_counts
+    new_photo = Photo()
+    new_photo.owner = user_id
+    new_photo.stream_name = stream_name
+    new_photo.title = title
+    new_photo.comment = comment
+    new_photo.content = content
+    new_photo.img_id = img_id + 1
+    user_data.photo_counts = img_id + 1
+    user_data.put()
+    new_photo.put()
+
+
+def get_photo(user_id, img_id):
+    photo = Photo.query(Photo.img_id == int(img_id), Photo.owner == user_id)
+    return photo.get()
+
+
+def get_photo_id_for_user(user_id):
+    photos = Photo.query(Photo.owner == user_id)
+    photo_id_list = []
+    for photo in photos.fetch(6):
+        photo_id_list.append(photo.img_id)
+    return photo_id_list
