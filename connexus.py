3a4496f9b90e06c6889bb20aa3ac8665b16756da
UTPatrick <lordbruce727@gmail.com>
2017-10-05 16:23:31 -0500

    Adding Search function - fix #20 (#29)
    * fixing all 2
    
    * fixing all 3

--- connexus.py
+++ connexus.py
@@ -10,7 +10,7 @@ import re
 from google.appengine.api import users
 from google.appengine.ext import ndb
 from google.appengine.api import mail
-
+from google.appengine.api import search
 
 import jinja2
 import webapp2
@@ -50,11 +50,17 @@ class MainLoginPage(webapp2.RequestHandler):
 # [START manage page]
 class ManagePage(webapp2.RequestHandler):
 
+    def post(self):
+        user = users.get_current_user()
+        stream_name = self.request.get_all('delete_status')
+        model.delete_stream(stream_name, user.user_id())
+        time.sleep(1)
+        self.redirect('/manage')
+
     def get(self):
         user = users.get_current_user()
-        streams = model.get_stream_by_user(user.user_id())
+        streams = model.get_stream_list_by_user(user.user_id())
         sub_streams = model.get_subscribed_stream(user.user_id())
-
         template_value = {
             'streams': streams,
             'sub_streams': sub_streams,
@@ -108,14 +114,15 @@ class ViewPage(webapp2.RequestHandler):
 
     def get(self):
         user = users.get_current_user()
-        stream_lists = model.get_stream_name_by_user(user.user_id())
+        streams = model.get_all_stream()
         template_value = {
             'greeting': 'this is the view page',
-            'stream_ids': stream_lists
+            'streams': streams
         }
         template = JINJA_ENVIRONMENT.get_template('templates/view_all_page.html')
         self.response.write(template.render(template_value))
 # [END view all page]
+
 
 # [START view one page]
 class ViewOnePage(webapp2.RequestHandler):
@@ -123,23 +130,28 @@ class ViewOnePage(webapp2.RequestHandler):
     def post(self):
         user = users.get_current_user()
         stream_name = self.request.get('stream')
-        print("yo")
-        print(stream_name)
         comment = self.request.get('comment')
         title = self.request.get('title')
         content = self.request.get('img')
-        model.add_photo(user.user_id(), stream_name, title, comment, content)
+        status = self.request.get('submit_btn')
+        if status == "Upload this photo":
+            model.add_photo(user.user_id(), stream_name, title, comment, content)
+        elif status == "Subscribe this stream":
+            print("subscription success!")
         # should use ancestor query, will change it later
         time.sleep(1)
         self.redirect('/view_one?stream=' + stream_name)
 
     def get(self):
         user = users.get_current_user()
-        temp = model.get_user(user.user_id())
-        temp.put()
         stream_name = self.request.get('stream')
-        photo_ids = model.get_photo_id_for_user(user.user_id())
+        photo_ids = model.get_photo_by_stream(stream_name, user.user_id())
+        print(model.get_stream_by_name(stream_name).owner)
+        print(model.get_user(user.user_id()).key)
+        is_owner = model.get_stream_by_name(stream_name).owner == model.get_user(user.user_id()).key
+        print(is_owner)
         template_value = {
+            'is_owner': is_owner,
             'greeting': 'this is the view page',
             'img_ids': photo_ids,
             'stream_name': stream_name
@@ -152,11 +164,24 @@ class ViewOnePage(webapp2.RequestHandler):
 # [START search page]
 class SearchPage(webapp2.RequestHandler):
 
+    def post(self):
+        keyword = self.request.get('search_str')
+        self.redirect('/search?keyword=' + keyword)
+
     def get(self):
+        user = users.get_current_user()
+        keyword = self.request.get('keyword')
+
+        if keyword:
+            x = model.search_stream(keyword)
+            print(x)
+        else:
+            x = []
         template_value = {
-            'greeting': 'this is the search page'
+            'greeting': 'this is the search page',
+            'stream_list': x
         }
-        template = JINJA_ENVIRONMENT.get_template('templates/page_template.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/search_page.html')
         self.response.write(template.render(template_value))
 
 # [END search page]
@@ -166,10 +191,11 @@ class SearchPage(webapp2.RequestHandler):
 class TrendingPage(webapp2.RequestHandler):
 
     def get(self):
+        user = users.get_current_user()
         template_value = {
             'greeting': 'this is the trending page'
         }
-        template = JINJA_ENVIRONMENT.get_template('templates/page_template.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/trending_page.html')
         self.response.write(template.render(template_value))
 # [END trending page]
 
@@ -181,7 +207,7 @@ class SocialPage(webapp2.RequestHandler):
         template_value = {
             'greeting': 'this is the social page'
         }
-        template = JINJA_ENVIRONMENT.get_template('templates/page_template.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/social_page.html')
         self.response.write(template.render(template_value))
 # [END social page]
 
@@ -202,12 +228,11 @@ class Image(webapp2.RequestHandler):
 
     def get(self):
         user = users.get_current_user()
-        img_id = self.request.get('img_id')
-        if img_id:
-            print(img_id)
-            photo = model.get_photo(user.user_id(), img_id)
+        img_id_url_safe = self.request.get('img_id')
+        if img_id_url_safe:
+            photo = ndb.Key(urlsafe=img_id_url_safe)
             self.response.headers['Content-Type'] = 'image/png'
-            self.response.out.write(photo.content)
+            self.response.out.write(photo.get().content)
         else:
             self.response.out.write('No image')
 
--- model.py
+++ model.py
@@ -1,139 +1,153 @@
 from google.appengine.api import images
 from google.appengine.ext import ndb
+from google.appengine.api import search
+from google.appengine.api import app_identity
+
 import os
 import webapp2
 import time
 
-from google.appengine.api import app_identity
+
+class User(ndb.Model):
+    user_id = ndb.StringProperty()
+    subscribes_list = ndb.StringProperty(repeated=True)
+    photo_counts = ndb.IntegerProperty()
+
+
+class Stream(ndb.Model):
+    # stream_name is unique meaning (among all users)
+    owner = ndb.KeyProperty(kind=User)
+    stream_name = ndb.StringProperty()
+    last_update = ndb.DateTimeProperty(auto_now=True)
+    photo_counts = ndb.IntegerProperty()
+    cover_image = ndb.StringProperty()
+    nums_of_view = ndb.IntegerProperty()
+    tags = ndb.StringProperty()
+
 
 class Photo(ndb.Model):
-    owner = ndb.StringProperty()
-    img_id = ndb.IntegerProperty()
-    stream_name = ndb.StringProperty()
+    up_stream = ndb.KeyProperty(kind=Stream)
     title = ndb.StringProperty()
     comment = ndb.StringProperty()
     last_update = ndb.DateTimeProperty(auto_now_add=True)
     content = ndb.BlobProperty()
 
 
-class Stream(ndb.Model):
-    # stream_name is unique meaning
-    owner = ndb.StringProperty()
-    stream_name = ndb.StringProperty()
-    last_update = ndb.DateTimeProperty(auto_now=True)
-    tag = ndb.StringProperty()
-    photo_counts = ndb.IntegerProperty()
-
-
-class User(ndb.Model):
-    user_id = ndb.StringProperty()
-    stream_list = ndb.StructuredProperty(Stream, repeated=True)
-    subscribe_list = ndb.StringProperty(repeated=True)
-    photo_counts = ndb.IntegerProperty()
+def create_user(id):
+    user = User(user_id=id, subscribes_list=[], photo_counts=0)
+    user.put()
+    return user
 
 
 def get_user(id):
     curr_user = User.query(User.user_id == id)
     user = curr_user.get()
     if not user:
-        user = User()
-        user.user_id=id
-        user.subscribe_list = []
-        user.stream_list = []
-        user.photo_counts = 0
-        user.put()
+        user = create_user(id)
     return user
 
 
-def get_stream_by_user(id):
-    user = get_user(id)
-    stream = Stream.query(Stream.owner == user.user_id)
+def get_stream_by_name(stream_name):
+    stream = Stream.query(Stream.stream_name == stream_name)
+    return stream.get()
+
+
+def get_all_stream():
+    stream = Stream.query()
     return stream.fetch()
 
-def get_stream_name_by_user(id):
+
+def get_stream_list_by_user(id):
     user = get_user(id)
-    user_stream_list = get_stream_by_user(user.user_id)
-    stream_name_list = []
-    for stream in user_stream_list:
-        stream_name_list.append(stream.stream_name)
-    return stream_name_list
+    stream = Stream.query(Stream.owner == user.key)
+    return stream.fetch()
+
 
 def get_subscribed_stream(id):
+    user = get_user(id)
+    subscribed_list = []
+    for sub_id in user.subscribes_list:
+        subscribed_list.append(sub_id)
+    return subscribed_list
+
+
+'''
+def get_subscriber(stream_name):
     user = get_user(id)
     list = []
     for sub_id in user.subscribe_list:
         sub = get_user(sub_id)
-        list.append(sub.stream_list)
+        #list.append(sub.stream_list)
     return list
-
+'''
 
 # search function
-def search_stream(stream_name):
-    streams = Stream.query(Stream.stream_name == stream_name)
+def search_stream(keyword):
+    streams = Stream.query(ndb.OR(Stream.stream_name == keyword, Stream.tags == keyword))
     stream_list = streams.fetch()
-    stream_list.sort(key=lambda x: x.photo_counts)
-    stream_top5 = []
-    for i in range(0,5):
-        stream_top5.append(stream_list[i])
-    return stream_top5
+    return stream_list
 
 
-def get_photo_by_stream(stream_name, user_id):
-    photos = Photo.query(Photo.stream_name == stream_name, Photo.owner == user_id)
+def get_photo_by_stream(stream_name, id):
+    user = get_user(id)
+    stream = get_stream_by_name(stream_name)
+    photos = Photo.query(Photo.up_stream == stream.key)
     photo_list = []
     for photo in photos.fetch():
-        photo_list.append(photo.content)
+        photo_list.append(photo)
     return photo_list
 
 
-def create_stream(stream_name, sub, tag, user_id):
-    user_data = get_user(user_id)
-    new_stream = Stream()
-    new_stream.tag = tag
-    new_stream.owner = user_id
-    new_stream.stream_name = stream_name
-    new_stream.photo_counts = 0
-    user_data.stream_list.append(new_stream)
-    user_data.subscribe_list.append(sub)
-    user_data.put()
+def create_stream(stream_name, sub, tag, id):
+    user = get_user(id)
+    new_stream = Stream(tags=tag, owner=user.key, stream_name=stream_name, photo_counts=0, nums_of_view=0)
+    user.put()
     new_stream.put()
 
 
-def add_photo(user_id, stream_name, title, comment, content):
-    user_data = get_user(user_id)
-    stream_name_list = get_stream_name_by_user(user_data.user_id)
-    if stream_name not in stream_name_list:
-        create_stream(stream_name, '', '', user_id)
-    img_id = user_data.photo_counts
-    new_photo = Photo()
-    new_photo.owner = user_id
-    new_photo.stream_name = stream_name
-
-    # should use ancestor query, will change it later
-    time.sleep(1)
-    stream = Stream.query(Stream.stream_name == stream_name)
-    user_stream = stream.get()
-    user_stream.photo_counts += 1
-
-    new_photo.title = title
-    new_photo.comment = comment
-    new_photo.content = content
-    new_photo.img_id = img_id + 1
-    user_data.photo_counts += 1
-
-    user_stream.put()
-    user_data.put()
+def add_photo(id, stream_name, title, comment, content):
+    user = get_user(id)
+    stream = get_stream_by_name(stream_name)
+    stream.photo_counts += 1
+    user.photo_counts += 1
+    new_photo = Photo(up_stream=stream.key, title=title, comment=comment, content=content)
     new_photo.put()
+    stream.put()
+    user.put()
 
 
-def get_photo(user_id, img_id):
-    photo = Photo.query(Photo.img_id == int(img_id), Photo.owner == user_id)
-    return photo.get()
+def get_photo(img_key):
+    photo = img_key.get()
+    return photo
 
 
 def get_photo_id_for_user(user_id):
-    photos = Photo.query(Photo.owner == user_id)
+    user = get_user(id)
+    photos = Photo.query(Photo.owner == user.key)
     photo_id_list = []
     for photo in photos.fetch(6):
         photo_id_list.append(photo.img_id)
     return photo_id_list
+
+
+def delete_photo_by_stream(stream_name, id):
+    user = get_user(id)
+    stream = get_stream_by_name(stream_name)
+    tbdPhotoQ = Photo.query(Photo.up_stream == stream.key)
+    tbdPhotoKey = tbdPhotoQ.get().key
+    tbdPhotoKey.delete()
+
+
+def delete_stream(stream_name_list, id):
+    user = get_user(id)
+    count = 0
+    for stream_name in stream_name_list:
+        tbdStreamQ = Stream.query(Stream.stream_name == stream_name, Stream.owner == user.key)
+        tbdStreamKey = tbdStreamQ.get().key
+        photo_counts = tbdStreamQ.get().photo_counts
+        if photo_counts != 0:
+            delete_photo_by_stream(stream_name, user.user_id)
+            count += photo_counts
+        tbdStreamKey.delete()
+    user.photo_counts -= count
+    user.put()
