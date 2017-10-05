7e08b4ee1bc05fa2978aeb2f92817b64954a5140
SHEN_XIAOCHENG <845876310@qq.com>
2017-10-05 15:57:17 -0500

    Have added email function in CreatePage (#23)
    * modifying functions
    
    * little tuning
    
    * view_all and view_one page separation
    
    * view_one_page update
    
    * add email functino in create page
    
    * add email function

--- connexus.py
+++ connexus.py
@@ -4,6 +4,8 @@
 import os
 import urllib
 import model
+import time
+import re
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
@@ -20,9 +22,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 # [END imports]
 
 # [START mainlogin page]
-DEFAULT_STREAM_NAME = 'new stream'
-
-
+DEFAULT_STREAM_NAME = 'new_stream'
 
 class MainLoginPage(webapp2.RequestHandler):
 
@@ -69,18 +69,27 @@ class CreatePage(webapp2.RequestHandler):
 
     def post(self):
         user = users.get_current_user()
-        stream_name = self.request.get('streamname', DEFAULT_STREAM_NAME)
+        stream_name = self.request.get('stream_name', DEFAULT_STREAM_NAME)
         sub = self.request.get('sub', '')
+        r = re.compile(r"[\w\.-]+@[\w\.-]+")
+        email_list = r.findall(sub)
         tag = self.request.get('tag', '')
         model.create_stream(stream_name, sub, tag, user.user_id())
+
+        email_content = self.request.get('email_content')
+        for subscriber in email_list:
+            mail.send_mail(sender = "xiaocs1@utexas.com",to = subscriber,subject = "Welcome to Connexus!",body = email_content)
 
         template_value = {
             'greeting': 'this is the create page'
         }
-        template = JINJA_ENVIRONMENT.get_template('templates/page_template.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/create_page.html')
         self.response.write(template.render(template_value))
+        time.sleep(1)
+        self.redirect('/manage')
 
     def get(self):
+        user = users.get_current_user()
         template_value = {
             'greeting': 'this is the the create page'
         }
@@ -89,34 +98,55 @@ class CreatePage(webapp2.RequestHandler):
 # [END create page]
 
 
-# [START view page]
+# [START view all page]
 class ViewPage(webapp2.RequestHandler):
 
     def post(self):
         user = users.get_current_user()
-        stream_name = self.request.get('steamname', DEFAULT_STREAM_NAME)
+        stream_name = self.request.get('stream_id')
+        self.redirect('/view_one?stream=' + stream_name)
+
+    def get(self):
+        user = users.get_current_user()
+        stream_lists = model.get_stream_name_by_user(user.user_id())
+        template_value = {
+            'greeting': 'this is the view page',
+            'stream_ids': stream_lists
+        }
+        template = JINJA_ENVIRONMENT.get_template('templates/view_all_page.html')
+        self.response.write(template.render(template_value))
+# [END view all page]
+
+# [START view one page]
+class ViewOnePage(webapp2.RequestHandler):
+
+    def post(self):
+        user = users.get_current_user()
+        stream_name = self.request.get('stream')
+        print("yo")
+        print(stream_name)
         comment = self.request.get('comment')
         title = self.request.get('title')
         content = self.request.get('img')
         model.add_photo(user.user_id(), stream_name, title, comment, content)
-        photo_ids = model.get_photo_id_for_user(user.user_id())
-        template_value = {
-            'greeting': 'this is the view page',
-            'img_ids': photo_ids
-        }
-        template = JINJA_ENVIRONMENT.get_template('templates/view_page.html')
-        self.response.write(template.render(template_value))
+        # should use ancestor query, will change it later
+        time.sleep(1)
+        self.redirect('/view_one?stream=' + stream_name)
 
     def get(self):
         user = users.get_current_user()
+        temp = model.get_user(user.user_id())
+        temp.put()
+        stream_name = self.request.get('stream')
         photo_ids = model.get_photo_id_for_user(user.user_id())
         template_value = {
             'greeting': 'this is the view page',
-            'img_ids': photo_ids
+            'img_ids': photo_ids,
+            'stream_name': stream_name
         }
-        template = JINJA_ENVIRONMENT.get_template('templates/view_page.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/view_one_page.html')
         self.response.write(template.render(template_value))
-# [END view page]
+# [END view one page]
 
 
 # [START search page]
@@ -188,6 +218,7 @@ app = webapp2.WSGIApplication([
     ('/manage', ManagePage),
     ('/create', CreatePage),
     ('/view', ViewPage),
+    ('/view_one', ViewOnePage),
     ('/search', SearchPage),
     ('/trending', TrendingPage),
     ('/social', SocialPage),
--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from google.appengine.api import images
 from google.appengine.ext import ndb
 import os
 import webapp2
+import time
 
 from google.appengine.api import app_identity
 
@@ -17,8 +18,9 @@ class Photo(ndb.Model):
 
 class Stream(ndb.Model):
     # stream_name is unique meaning
+    owner = ndb.StringProperty()
     stream_name = ndb.StringProperty()
-    last_update = ndb.DateTimeProperty(auto_now_add=True)
+    last_update = ndb.DateTimeProperty(auto_now=True)
     tag = ndb.StringProperty()
     photo_counts = ndb.IntegerProperty()
 
@@ -39,13 +41,22 @@ def get_user(id):
         user.subscribe_list = []
         user.stream_list = []
         user.photo_counts = 0
+        user.put()
     return user
 
 
 def get_stream_by_user(id):
     user = get_user(id)
-    return user.stream_list
+    stream = Stream.query(Stream.owner == user.user_id)
+    return stream.fetch()
 
+def get_stream_name_by_user(id):
+    user = get_user(id)
+    user_stream_list = get_stream_by_user(user.user_id)
+    stream_name_list = []
+    for stream in user_stream_list:
+        stream_name_list.append(stream.stream_name)
+    return stream_name_list
 
 def get_subscribed_stream(id):
     user = get_user(id)
@@ -54,6 +65,17 @@ def get_subscribed_stream(id):
         sub = get_user(sub_id)
         list.append(sub.stream_list)
     return list
+
+
+# search function
+def search_stream(stream_name):
+    streams = Stream.query(Stream.stream_name == stream_name)
+    stream_list = streams.fetch()
+    stream_list.sort(key=lambda x: x.photo_counts)
+    stream_top5 = []
+    for i in range(0,5):
+        stream_top5.append(stream_list[i])
+    return stream_top5
 
 
 def get_photo_by_stream(stream_name, user_id):
@@ -68,24 +90,38 @@ def create_stream(stream_name, sub, tag, user_id):
     user_data = get_user(user_id)
     new_stream = Stream()
     new_stream.tag = tag
-    new_stream.user_id = user_id
+    new_stream.owner = user_id
     new_stream.stream_name = stream_name
+    new_stream.photo_counts = 0
     user_data.stream_list.append(new_stream)
     user_data.subscribe_list.append(sub)
     user_data.put()
+    new_stream.put()
 
 
 def add_photo(user_id, stream_name, title, comment, content):
     user_data = get_user(user_id)
+    stream_name_list = get_stream_name_by_user(user_data.user_id)
+    if stream_name not in stream_name_list:
+        create_stream(stream_name, '', '', user_id)
     img_id = user_data.photo_counts
     new_photo = Photo()
     new_photo.owner = user_id
     new_photo.stream_name = stream_name
+
+    # should use ancestor query, will change it later
+    time.sleep(1)
+    stream = Stream.query(Stream.stream_name == stream_name)
+    user_stream = stream.get()
+    user_stream.photo_counts += 1
+
     new_photo.title = title
     new_photo.comment = comment
     new_photo.content = content
     new_photo.img_id = img_id + 1
-    user_data.photo_counts = img_id + 1
+    user_data.photo_counts += 1
+
+    user_stream.put()
     user_data.put()
     new_photo.put()
 
